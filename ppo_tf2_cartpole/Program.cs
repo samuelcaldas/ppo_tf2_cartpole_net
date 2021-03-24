c45b2e4275735e91c344bb47ed770531c4b9be07
Samuel Caldas <samuel.caldas@gmail.com>
2021-03-24 01:59:27 -0300

    More changes

--- ppo_tf2_cartpole/Program.cs
+++ ppo_tf2_cartpole/Program.cs
@@ -10,6 +10,14 @@ using Gym.Environments.Envs.Classic;
 using Gym.Rendering.WinForm;
 //
 using NumSharp;
+using Tensorflow;
+using Tensorflow.Keras;
+using Tensorflow.Keras.ArgsDefinition;
+using Tensorflow.Keras.Engine;
+using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Losses;
+using Tensorflow.Keras.Optimizers;
+using Tensorflow.Keras.Utils;
 using static Tensorflow.Binding;
 using static Tensorflow.KerasApi;
 //import tensorflow as tf
@@ -21,33 +29,38 @@ using static Tensorflow.KerasApi;
 
 namespace ppo_tf2_cartpole
 {
-    class Model
+    public class KerasModelArgs : ModelArgs
+    {
+        public int NumActions { get; set; }
+    }
+    public class KerasModel : Model
     { //(keras.Model)
-        var dense1;
-        var dense2;
-        var value;
-        var policy_logits;
-        Model(var num_actions)
+        Layer dense1;
+        Layer dense2;
+        Layer value;
+        Layer policy_logits;
+        public KerasModel(KerasModelArgs args)
+            : base(args)
         {
             //super().__init__();
             dense1 = keras.layers.Dense(64, activation: keras.activations.Relu,
-                                             kernel_initializer: keras.initializers.he_normal());
+                                        kernel_initializer: keras.initializers.he_normal());
             dense2 = keras.layers.Dense(64, activation: keras.activations.Relu,
-                                             kernel_initializer: keras.initializers.he_normal());
+                                        kernel_initializer: keras.initializers.he_normal());
             value = keras.layers.Dense(1);
-            policy_logits = keras.layers.Dense(num_actions);
+            policy_logits = keras.layers.Dense(args.NumActions);
         }
-        static void call(var inputs)
+        protected override Tensors Call(Tensors inputs, Tensor state = null, bool? training = null)
         {
-            var x = dense1(inputs);
-            x = dense2(x);
+            Tensor x = dense1.Apply(inputs);
+            x = dense2.Apply(x);
             return (value.Apply(x), policy_logits.Apply(x));
         }
-        static void action_value(var state)
+        public (Tensor, Tensor) action_value(Tensor state)
         {
-            var (value, logits) = this.predict_on_batch(state);
-            var dist = tfp.distributions.Categorical(logits: logits);
-            var action = dist.sample();
+            var (value, logits) = predict_on_batch(state);
+            Tensor dist = tfp.distributions.Categorical(logits: logits);
+            Tensor action = dist.sample();
             return (action, value);
         }
     }
@@ -64,62 +77,67 @@ namespace ppo_tf2_cartpole
 
         static int NUM_TRAIN_EPOCHS = 10;
 
-        //static var env = gym.make("CartPole-v0");
-        CartPoleEnv env = new CartPoleEnv(WinFormEnvViewer.Factory);
+        //static Tensor env = gym.make("CartPole-v0");
+        static CartPoleEnv env = new CartPoleEnv(WinFormEnvViewer.Factory);
         static int state_size = 4;
-        static int num_actions = env.action_space.n;
+        static int num_actions = env.ActionSpace.Shape.NDim;
 
         static double ent_discount_val = ENTROPY_LOSS_WEIGHT;
 
-        static void critic_loss(var discounted_rewards, var value_est)
+        static Model model = new KerasModel(new KerasModelArgs
         {
-            return tf.cast(tf.reduce_mean(keras.losses.mean_squared_error(discounted_rewards, value_est)) * CRITIC_LOSS_WEIGHT,
+            NumActions = num_actions,
+        });
+
+        static Tensors critic_loss(Tensor discounted_rewards, Tensor value_est)
+        {
+            return tf.cast(tf.reduce_mean(keras.losses.MeanSquaredError().Call(discounted_rewards, value_est)) * CRITIC_LOSS_WEIGHT,
                            tf.float32);
         }
-        static void entropy_loss(var policy_logits, var ent_discount_val)
+        static Tensors entropy_loss(Tensor policy_logits, Tensor ent_discount_val)
         {
-            var probs = tf.nn.softmax(policy_logits);
-            var entropy_loss = -tf.reduce_mean(keras.losses.categorical_crossentropy(probs, probs));
+            Tensor probs = tf.nn.softmax(policy_logits);
+            Tensor entropy_loss = -tf.reduce_mean(keras.losses.CategoricalCrossentropy().Call(probs, probs));
             return entropy_loss * ent_discount_val;
         }
-        static void actor_loss(var advantages, var old_probs, var action_inds, var policy_logits)
+        static Tensors actor_loss(Tensor advantages, Tensor old_probs, Tensor action_inds, Tensor policy_logits)
         {
-            var probs = tf.nn.softmax(policy_logits);
-            var new_probs = tf.gather_nd(probs, action_inds);
+            Tensor probs = tf.nn.softmax(policy_logits);
+            Tensor new_probs = tf.gather(probs, action_inds);
 
-            var ratio = new_probs / old_probs;
+            Tensor ratio = new_probs / old_probs;
 
-            var policy_loss = -tf.reduce_mean(tf.math.minimum(
-            ratio * advantages,
-            tf.clip_by_value(ratio, 1.0 - CLIP_VALUE, 1.0 + CLIP_VALUE) * advantages
-        ));
+            Tensor policy_loss = -tf.reduce_mean(np.minimum(ratio * advantages,
+                                                            tf.clip_by_value(ratio, 1.0 - CLIP_VALUE, 1.0 + CLIP_VALUE) * advantages));
             return policy_loss;
         }
-        static void train_model(var action_inds, var old_probs, var states, var advantages, var discounted_rewards, var optimizer, var ent_discount_val)
+        static (Tensor, Tensor, Tensor, Tensor) train_model(Tensor action_inds, Tensor old_probs, Tensor states, Tensor advantages, Tensor discounted_rewards, Tensor optimizer, Tensor ent_discount_val)
         {
+            Tensor tot_loss;
             using (var tape = tf.GradientTape())
             {
-                var (values, policy_logits) = model.call(tf.stack(states));
-                var act_loss = actor_loss(advantages, old_probs, action_inds, policy_logits);
-                var ent_loss = entropy_loss(policy_logits, ent_discount_val);
-                var c_loss = critic_loss(discounted_rewards, values);
-                var tot_loss = act_loss + ent_loss + c_loss;
+                var modelpredict = model.Call(tf.stack(states));
+                var (values, policy_logits) = (modelpredict[0], modelpredict[1]);
+                Tensor act_loss = actor_loss(advantages, old_probs, action_inds, policy_logits);
+                Tensor ent_loss = entropy_loss(policy_logits, ent_discount_val);
+                Tensor c_loss = critic_loss(discounted_rewards, values);
+                Tensor tot_loss = act_loss + ent_loss + c_loss;
             }
-            var grads = tape.gradient(tot_loss, model.trainable_variables);
+            Tensor grads = tape.gradient(tot_loss, model.trainable_variables);
             optimizer.apply_gradients(zip(grads, model.trainable_variables));
             return (tot_loss, c_loss, act_loss, ent_loss);
         }
-        static void get_advantages(var rewards, var dones, var values, var next_value)
+        static (Tensor, Tensor) get_advantages(Tensor rewards, Tensor dones, NDArray values, Tensor next_value)
         {
-            var discounted_rewards = np.array(rewards + [next_value[0]]);
+            Tensor discounted_rewards = np.array(rewards + (next_value[0]));
 
-            for (var t in reversed(range(len(rewards))))
+            for (int t = 0; reversed(range(len(rewards))))
             {
                 discounted_rewards[t] = rewards[t] + GAMMA * discounted_rewards[t + 1] * (1 - dones[t]);
             }
             discounted_rewards = discounted_rewards[":-1"];
             // advantages are bootstrapped discounted rewards - values, using Bellman's equation
-            var advantages = discounted_rewards - np.stack(values)[":, 0"];
+            NDArray advantages = discounted_rewards - np.stack(values)[":, 0"];
             // standardise advantages
             advantages -= np.mean(advantages);
             advantages /= (np.std(advantages) + 1e-10);
@@ -130,17 +148,17 @@ namespace ppo_tf2_cartpole
         }
         static void Main(string[] args)
         {
-            var model = new Model(num_actions);
-            var optimizer = keras.optimizers.Adam(learning_rate: LR);
 
-            var train_writer = tf.summary.create_file_writer(STORE_PATH + f"/PPO-CartPole_{dt.datetime.now().strftime(" % d % m % Y % H % M")}");
+            Tensor optimizer = keras.optimizers.Adam(learning_rate: LR);
 
-            var num_steps = 10000000;
-            var episode_reward_sum = 0;
-            var state = env.reset();
-            var episode = 1;
-            var total_loss = Null;
-            for (var step in range(num_steps))
+            Tensor train_writer = tf.summary.create_file_writer(STORE_PATH + f"/PPO-CartPole_{dt.datetime.now().strftime(" % d % m % Y % H % M")}");
+
+            int num_steps = 10000000;
+            Double episode_reward_sum = 0;
+            Tensor state = env.Reset();
+            int episode = 1;
+            Double total_loss = 0;
+            for (int step = 0; step < num_steps; step++)
             {
                 var rewards = new List<double>();
                 var actions = new List<double>();
@@ -148,12 +166,12 @@ namespace ppo_tf2_cartpole
                 var states = new List<double>();
                 var dones = new List<double>();
                 var probs = new List<double>();
-                for (var _ in range(BATCH_SIZE))
+                for (int i = 0; i < BATCH_SIZE; i++)
                 {
                     var (_, policy_logits) = model(state.reshape(1, -1));
 
                     var (action, value) = model.action_value(state.reshape(1, -1));
-                    var (new_state, reward, done, _) = env.step(action.numpy()[0]);
+                    var (new_state, reward, done, _) = env.Step(action.numpy()[0]);
 
                     actions.Add(action);
                     values.Add(value[0]);
@@ -167,12 +185,12 @@ namespace ppo_tf2_cartpole
                     if (done)
                     {
                         rewards.Add(0.0);
-                        state = env.reset();
-                        if (total_loss is not Null)
+                        state = env.Reset();
+                        if (total_loss != 0)
                         {
-                            Console.WriteLine(f"Episode: {episode}, latest episode reward: {episode_reward_sum}, "
-                                  f"total loss: {np.mean(total_loss)}, critic loss: {np.mean(c_loss)}, "
-                                  f"actor loss: {np.mean(act_loss)}, entropy loss {np.mean(ent_loss)}");
+                            Console.WriteLine("Episode: {episode}, latest episode reward: {episode_reward_sum}, ",
+                                              "total loss: {np.mean(total_loss)}, critic loss: {np.mean(c_loss)}, ",
+                                              "actor loss: {np.mean(act_loss)}, entropy loss {np.mean(ent_loss)}");
                         }
                         using (train_writer.as_default())
                         {
@@ -192,15 +210,15 @@ namespace ppo_tf2_cartpole
 
                 actions = tf.squeeze(tf.stack(actions));
                 probs = tf.nn.softmax(tf.squeeze(tf.stack(probs)));
-                var action_inds = tf.stack([tf.range(0, actions.shape[0]), tf.cast(actions, tf.int32)], axis:1);
+                Tensor action_inds = tf.stack((tf.range(0, actions.shape[0]), tf.cast(actions, tf.int32)), axis: 1);
 
                 total_loss = np.zeros((NUM_TRAIN_EPOCHS));
-                var act_loss = np.zeros((NUM_TRAIN_EPOCHS));
-                var c_loss = np.zeros(((NUM_TRAIN_EPOCHS)));
-                var ent_loss = np.zeros((NUM_TRAIN_EPOCHS));
-                for (var epoch in range(NUM_TRAIN_EPOCHS))
+                Tensor act_loss = np.zeros((NUM_TRAIN_EPOCHS));
+                Tensor c_loss = np.zeros(((NUM_TRAIN_EPOCHS)));
+                Tensor ent_loss = np.zeros((NUM_TRAIN_EPOCHS));
+                for (int epoch = 0; epoch < NUM_TRAIN_EPOCHS; epoch++)
                 {
-                    var loss_tuple = train_model(action_inds, tf.gather_nd(probs, action_inds),
+                    Tensor loss_tuple = train_model(action_inds, tf.gather(probs, action_inds),
                                              states, advantages, discounted_rewards, optimizer,
                                              ent_discount_val);
                     total_loss[epoch] = loss_tuple[0];
@@ -208,7 +226,7 @@ namespace ppo_tf2_cartpole
                     act_loss[epoch] = loss_tuple[2];
                     ent_loss[epoch] = loss_tuple[3];
                 }
-                var ent_discount_val *= ENT_DISCOUNT_RATE;
+                Tensor ent_discount_val *= ENT_DISCOUNT_RATE;
 
                 using (train_writer.as_default())
                 {
